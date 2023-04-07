18dac4e91a4fa92390966a95812a2d1690138674
github-hugo-vincent <hugo.vincent@teads.tv>
2023-04-07 21:11:09 +0200

    connection to github app

--- githubapp/main.js
+++ githubapp/main.js
@@ -1,19 +1,30 @@
-const { App, createNodeMiddleware } = require("@octokit/app");
+const { createAppAuth } = require("@octokit/auth-app");
+const { Octokit, App } = require( "octokit");
 
-// Your GitHub app's authentication details
-const appId = "YOUR_APP_ID";
-const privateKey = "YOUR_APP_PRIVATE_KEY";
-const installationId = "YOUR_APP_INSTALLATION_ID";
-const clientID = "YOUR_APP_CLIENT_ID";
+// const owner = 'Hy0g0'; // Replace with the owner of the repository
+// const repo = 'CiEpitech'; // Replace with the name of the repository
 
-const app = new App({
+const token = process.env.GITHUB_PRIVATE_KEY
+const appId = process.env.GITHUB_APP_ID
+const installId = process.env.GITHUB_INSTALLATION_ID
+
+
+const octokit = new Octokit({
+  authStrategy: createAppAuth,
+  auth: {
     appId: appId,
-    privateKey: privateKey,
-    oauth: {
-      clientId: clientID,
-      clientSecret: privateKey,
-    },
-    webhooks: {
-      secret: "secret",
-    },
-  });
+    privateKey: token,
+    installationId: installId,
+  },
+});
+
+async function main() {
+const {
+  data: { slug },
+} = await octokit.rest.apps.getAuthenticated();
+console.log("Hello, %s", slug);
+
+
+}
+
+main()
