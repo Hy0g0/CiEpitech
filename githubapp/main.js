4b9d65698fb3396ab5d7e6a0da317069731cb27b
github-hugo-vincent <hugo.vincent@teads.tv>
2023-04-09 00:31:43 +0200

    detect a push

--- githubapp/main.js
+++ githubapp/main.js
@@ -8,6 +8,7 @@ const token = process.env.GITHUB_PRIVATE_KEY
 const appId = process.env.GITHUB_APP_ID
 const installId = process.env.GITHUB_INSTALLATION_ID
 
+var lastPush = null
 
 const octokit = new Octokit({
   authStrategy: createAppAuth,
@@ -18,18 +19,41 @@ const octokit = new Octokit({
   },
 });
 
-async function main() {
+async function Authentification(){
 const {
   data: { slug },
 } = await octokit.rest.apps.getAuthenticated();
 console.log("Hello, %s", slug);
-
-await octokit.rest.issues.create({
-  owner: owner,
-  repo: repo,
-  title: "Hello world from " + slug,
-});
-
 }
 
-main()
+async function getBaseInfos(){
+  var { data } = await octokit.rest.activity.listRepoEvents({
+    owner,
+    repo
+  });
+
+  lastPush = data.filter(event => event.type === "PushEvent").shift();
+  console.log("Last push: %s", lastPush)
+}
+
+async function checkForPushEvent() {
+  var { data } = await octokit.rest.activity.listRepoEvents({
+    owner,
+    repo
+  });
+
+  const pushEvent = data.filter(event => event.type === "PushEvent").shift();
+  if (pushEvent.id !== lastPush.id) {
+    console.log("New push detected: %s", pushEvent);
+  } else {
+    console.log("No new push was detected");
+  }
+}
+Authentification()
+getBaseInfos()
+setInterval(async () => {
+  await checkForPushEvent();
+}, 10000); // Check every 10 seconds
+
+
+
