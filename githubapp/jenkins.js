4955f50e4936a45a186cbe5d77bd8876606da293
github-hugo-vincent <hugo.vincent@teads.tv>
2023-04-18 00:07:08 +0200

    push workflow for node app

--- githubapp/jenkins.js
+++ githubapp/jenkins.js
@@ -2,13 +2,13 @@ var jenkins = require("jenkins")
 
 const jenurl = process.env.JENKINS_URL
 
+
 const instance =  new jenkins({
   baseUrl: jenurl
 });
 
-async function main() {
-var test = await instance.info()
-console.log(test)
+exports.Authentification = async function Authentification() {
+var conn = await instance.info()
+console.log(conn.description)
+return instance
 }
-
-main();
