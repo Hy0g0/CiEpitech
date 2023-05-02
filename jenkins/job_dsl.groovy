7ca2c1aebf7138fb5a3fca622bde4b250aac1e2b
Alain Victor <alain-joseph-antoine.victor@epitech.eu>
2023-05-02 16:48:12 +0200

    Jenkins DinD

--- jenkins/job_dsl.groovy
+++ jenkins/job_dsl.groovy
@@ -31,8 +31,8 @@
            }
            steps {
             shell("git checkout \$GIT_BRANCH_NAME")
-            shell("echo 'Done!'")
-            //TODO: Launch tflocal Plan in Docker container
+            shell("docker run --name cilocal-localstack-1 -e LOCALSTACK_API_KEY=\${LOCALSTACK_API_KEY} -v \$(pwd)/AWS:/AWS localstack/localstack-pro tflocal -chdir=/AWS/ plan")
+            shell("docker run -v \$(pwd)/AWS:/AWS hashicorp/terraform -chdir=/AWS/ apply -auto-approve")
            }
        }
 
@@ -52,6 +52,10 @@
 //                       shell("git checkout \$GIT_BRANCH_NAME")
                       shell("echo 'Done!'")
                 }
+                steps {
+                            shell("git checkout \$GIT_BRANCH_NAME")
+                            shell("docker run --name cilocal-localstack-1 -e LOCALSTACK_API_KEY=\${LOCALSTACK_API_KEY} -v \$(pwd)/AWS:/AWS localstack/localstack-pro tflocal -chdir=/AWS/ apply -auto-approve")
+                           }
             }
             //TODO: Launch tflocal Apply in Docker container
        }
