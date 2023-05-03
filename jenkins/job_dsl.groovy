8c13641e19ae60e8132824f1c83e4ac77075cc47
Alain Victor <alain-joseph-antoine.victor@epitech.eu>
2023-05-03 22:26:36 +0200

    Adding tflocal cmd to jobs

--- jenkins/job_dsl.groovy
+++ jenkins/job_dsl.groovy
@@ -31,31 +31,24 @@
            }
            steps {
             shell("git checkout \$GIT_BRANCH_NAME")
-            shell("docker run --name cilocal-localstack-1 -e LOCALSTACK_API_KEY=\${LOCALSTACK_API_KEY} -v \$(pwd)/AWS:/AWS localstack/localstack-pro tflocal -chdir=/AWS/ plan")
-            shell("docker run -v \$(pwd)/AWS:/AWS hashicorp/terraform -chdir=/AWS/ apply -auto-approve")
+            shell("docker cp \$(pwd)/AWS/. 'ciepitech-localstack-1':/AWS")
+            shell("docker exec 'ciepitech-localstack-1'  tflocal -chdir=/AWS/ plan")
            }
        }
 
        freeStyleJob('/Tools/apply') {
-           parameters {
-                stringParam("GIT_BRANCH_NAME", "", "Github branch repository name")
-           }
-            scm {
-                steps {
-                     git {
-                         remote {
-                             url('https://github.com/Hy0g0/CiEpitech.git')
-                             credentials('Jenkins_Agent')
-                         }
-                         branch('\$GIT_BRANCH_NAME')
-                     }
-//                       shell("git checkout \$GIT_BRANCH_NAME")
-                      shell("echo 'Done!'")
-                }
-                steps {
-                            shell("git checkout \$GIT_BRANCH_NAME")
-                            shell("docker run --name cilocal-localstack-1 -e LOCALSTACK_API_KEY=\${LOCALSTACK_API_KEY} -v \$(pwd)/AWS:/AWS localstack/localstack-pro tflocal -chdir=/AWS/ apply -auto-approve")
-                           }
-            }
-            //TODO: Launch tflocal Apply in Docker container
+           steps {
+               scm {
+                   git{
+                       remote {
+                           url('https://github.com/Hy0g0/CiEpitech.git')
+                           credentials('Jenkins_Agent')
+                       }
+                   }
+               }
+          }
+          steps {
+           shell("docker cp \$(pwd)/AWS/. 'ciepitech-localstack-1':/AWS")
+           shell("docker exec 'ciepitech-localstack-1'  tflocal -chdir=/AWS/ apply -auto-approve")
+          }
        }
